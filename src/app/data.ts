e36f125f8b0f69aef44a8c65662994b4d876926b
Serzhio <kserzhio@gmail.com>
2020-03-01 18:12:02 +0200

    Add files via upload

---
+++ src/app/data.ts
@@ -0,0 +1,110 @@
+export const data = [
+  {
+    period: [
+      {
+        name: '1',
+        cell: '1-1'
+      },
+      {
+        name: '2',
+        cell: 2
+      },
+      {
+        name: '3',
+        cell: 3
+      },
+      {
+        name: '4',
+        cell: 4
+      },
+      {
+        name: '5',
+        cell: 5
+      },
+      {
+        name: '6',
+        cell: 6
+      },
+      {
+        name: '7',
+        cell: 7
+      },
+      {
+        name: '8',
+        cell: 8
+      },
+      {
+        name: '9',
+        cell: 9
+      },
+      {
+        name: '10',
+        cell: 10
+      },
+      {
+        name: '11',
+        cell: 11
+      },
+      {
+        name: '12',
+        cell: 12
+      },
+      {
+        name: '13',
+        cell: 13
+      }
+    ],
+    country: [
+      {
+        title: 'Belgium'
+      },
+      {
+        title: 'Belgium1'
+      },
+      {
+        title: 'Belgium2'
+      },
+      {
+        title: 'Belgium3'
+      }
+    ]
+  },
+  {
+    name: 'timeLine',
+    cell: 5,
+    checked: false,
+    rate: [
+      {
+        value: '1 rate'
+      },
+      {
+        value: '2 rate'
+      },
+      {
+        value: '3 timeLine '
+      },
+      {
+        value: '4 timeLine'
+      },
+    ]
+  },
+  {
+    name: 'timeLine2',
+    cell: 3,
+    checked: false,
+    rate: [
+      {
+        value: '1 rate'
+      },
+      {
+        value: '2 rate'
+      },
+      {
+        value: '3 timeLine2'
+      },
+      {
+        value: '4 timeLine2'
+      },
+    ]
+  }
+];
